f97afb1f2acdd328b310789df097aff91057a367
sjhan_windows <june12300039@gmail.com>
2026-04-28 04:07:48 +0900

    feat: tailwindcss 빌드 및 설정 추가

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./subject.html",
+    "./search.html",
+    "./predictor.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
